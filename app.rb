39b058ea39164a4f87979992db991dff68c27d62
Anthony Rurup <tcrurup@gmail.com>
2019-06-13 00:23:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+  
   get '/' do 
     erb :index
   end
   
   post '/checkout' do
-    
+    @sessions = session
   end
 end
